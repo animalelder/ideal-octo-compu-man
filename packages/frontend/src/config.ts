68906f39e54102ace59fa26db7528fc90502536c
animalelder <wllearned@icloud.com>
2024-10-09 16:37:28 -0700

    Building the Vite React App

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,4 +1,8 @@
 const config = {
+  // Frontend config
+  STRIPE_KEY:
+    'pk_test_51Q7qGBRwf7QLAYCIhjaQlFAzwT3AL36mpcgGtTbwsGhBhDd4KwCuWckqy8LUAjhvQ5O4se3j9uNXVW4ymaLlobRu00pEM0nP1u',
+  MAX_ATTACHMENT_SIZE: 5000000,
   // Backend config
   s3: {
     REGION: import.meta.env.VITE_REGION,
